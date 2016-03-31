a087a0599051d1fa92fd47ea6e1c666aac1fc4d0
lyt9304 <513535169@qq.com>
2016-03-31 16:28:54 +0800

    reform architecture

--- page/client.js
+++ page/client.js
@@ -1,67 +1,102 @@
 ;(function(){
 	var w = window;
-	w.watch={
-		pageId: null,
+	w.booster={
 		socket: null,
 		fileList: null,
 		mask: null,
-		getPageId: function () {
-			return new Date().getTime()+""+Math.floor(Math.random()*899+100);
-		},
-		init: function(fileList){
+		/**
+		 * 初始化booster
+		 * 1. 初始化loading遮罩模块
+		 * 2. 寻找该页面的所需要的资源
+		 * 3. 连接socket
+		 * 4. 监听change事件
+		 */
+		init: function(){
 			this.mask=new w.LivereloadMask();
-			this.fileList=fileList;
+			this.findFileList();
 			this.socket=io("http://localhost:3000");
-			this.pageId=this.getPageId();
-			this.register();
 			this.changeResource();
 		},
-		register: function(){
-			this.socket.emit("register",{pageId:this.pageId,fileList:this.fileList});
+		/**
+		 * 根据src和href中的url分析出对应的资源的名称
+		 * @param path 资源路径
+		 */
+		getResourceName: function (path) {
+			//去除可能的?后的参数
+			var pathName=path.replace(/\?.*$/,"");
+			var pathNameArr=pathName.split("/");
+			var len=pathNameArr.length;
+			return pathNameArr[len-1];
 		},
+		/**
+		 * 寻找该页面的所需要的资源(css,javascript)
+		 */
+		findFileList: function () {
+			var fileList=[];
+			//处理css资源
+			var links=document.getElementsByTagName("link");
+			for (var i = 0; i < links.length; i++) {
+				var link = links[i];
+				var href = link.href;
+				fileList.push({name:this.getResourceName(href),path:href,dom:link});
+			}
+
+			//处理js资源
+			var scripts=document.getElementsByTagName("script");
+			for (var i = 0; i < scripts.length; i++) {
+				var script = scripts[i];
+				var src = script.src;
+				fileList.push({name:this.getResourceName(src),path:src});
+			}
+
+			this.fileList=fileList;
+		},
+		/**
+		 * 监听资源变动的change事件
+		 */
 		changeResource: function () {
 			var that=this;
 			this.socket.on("change", function (data) {
-				//console.log(data);
-				var fileName=data.file;
-				var filePath=data.source;
-				if(/\.css/.test(fileName)){
-					that.changecss(fileName,filePath);
-				} else if (/\.js/.test(fileName)){
-					that.changejs(fileName,filePath);
-				} else {
-					console.log("未知类型的文件!");
+				var fileName=data.name;
+				var filePath=data.path;
+
+				//TODO:路径映射,不能简单匹配名字,需要一个双向映射的转换模块
+
+				for (var i = 0; i < that.fileList.length; i++) {
+					var file = that.fileList[i];
+					if(fileName===file.name){
+						if(/\.css/.test(fileName)){
+							that.changecss(file.dom,fileName);
+						} else if (/\.js/.test(fileName)){
+							that.changejs();
+						} else {
+							console.log("未知类型的文件!");
+						}
+						console.log("File: "+ fileName +" has been changed!");
+						return;
+					}
 				}
-				console.log("File: "+ fileName +" has been changed!");
+				console.log("This resource is not in this page!");
 			});
 		},
-		changecss: function (name,path) {
+		/**
+		 * 处理css的变化
+		 * @param name 变动的css文件的名字
+		 */
+		changecss: function (dom,name) {
 			this.mask.open();
-			var rname=new RegExp(name);
-			var links=document.getElementsByTagName("link");
-			for (var i = 0; i < links.length; i++) {
-				var obj = links[i];
-				var href = obj.href;
-				if(rname.test(href)){
-					obj.href=path;
-				}
-			}
-			console.log("css File: "+ name +" has been changed!");
+			var randomStamp=new Date().getTime()+""+Math.floor(Math.random()*899+100);
+			dom.href=dom.href+"?"+randomStamp;
 			this.mask.done(name);
 		},
-		changejs: function (name,path) {
-			var rname=new RegExp(name);
-			var scripts=document.getElementsByTagName("script");
-			for (var i = 0; i < scripts.length; i++) {
-				var obj = scripts[i];
-				var src = obj.src;
-				if(rname.test(src)){
-					obj.src=path;
-					console.log("src changed!");
-					console.log(obj);
-				}
-			}
-			console.log("js File: "+ name +" has been changed!");
+		/**
+		 * 处理js的变化
+		 * @param name
+		 * @param path
+		 */
+		changejs: function () {
+			this.mask.open();
+			w.location.reload();
 		}
 	}
 })();
--- server.js
+++ server.js
@@ -1,88 +1,31 @@
 /** Server
  * Created by lyt9304 on 16/3/29.
  */
+
+var chokidar=require("chokidar");
+var util=require("./util");
+var log=require("./log");
+var path=require("path");
+
+/**
+ * Server config and create
+ */
+var port=process.env.PORT || 3000;
 var server=require("http").createServer(function(req,res){
 	res.writeHead(200,{'Content-type':'text/plain'});
 	res.end("Hello World!\n");
 });
 var io=require("socket.io")(server);
-var port=process.env.PORT || 3000;
-var chokidar=require("chokidar");
-var util=require("./util");
-var log=require("./log");
-var path=require("path");
-var fs=require("fs")
-
 server.listen(port, function () {
 	console.log('Server listening at port %d', port);
 });
 
 /**
- * pageOffice记录现在保持连接的页面
- * @type {{}}
+ * Socket.io Server Handler
  */
-var pageOffice={};
-
-/**
- * pageCount记录现在保持连接的页面的数量
- * @type {number}
- */
-var pageCount=0;
-
-/**
- * resourceOffice 是管理页面以及其关注的文件的一个注册中心
- * key(文件)-value(对应页面id的数组)
- * @type {{}}
- */
-var resourceOffice={};
-
-/**
- * 加入新的页面
- * @param data:{pageId,fileList}
- */
-function addPage(data){
-	if(pageOffice.hasOwnProperty(data.pageId)){
-		log.error("已经存在该pageId!");
-		return;
-	}
-	pageOffice[data.pageId]=true;
-	pageCount++;
-
-	if(!util.isArray(data.fileList)){
-		log.error("需要数组形式的关注文件列表");
-		return;
-	}
-
-	//添加resouceOffice
-	for (var i = 0,len=data.fileList.length; i < len; i++) {
-		var fileItem = data.fileList[i];
-		if(!resourceOffice[fileItem]){
-			resourceOffice[fileItem]=[];
-		}
-		resourceOffice[fileItem].push(data.pageId);
-	}
-}
-
-/**
- * 当页面被关闭触发disconnect
- * @param name {String} socket标示符name,通过这个来删除这个socket的相关数据
- */
-function removePage(name){
-	if(!pageOffice.hasOwnProperty(name)){
-		log.error("没有找到该pageId!");
-		return;
-	}
-	delete pageOffice[name];
-	pageCount--;
-
-	//清理resourceOffice
-	for(var fileItem in resourceOffice){
-		var pageList=resourceOffice[fileItem];
-		util.removeAllElem(pageList,name);
-	}
-}
-
 io.on('connection',function(socket){
+
+	log.info("in connection!");
 
 	//初始化watcher文件监控器
 	var watcher = chokidar.watch(path.join(__dirname,"resource"), {
@@ -90,7 +33,6 @@ io.on('connection',function(socket){
 		persistent: true
 		//cwd:process.cwd()
 	});
-	watcher.unwatch(path.join(__dirname,"resource"));
 
 	//为watcher添加add/change/unlink(remove)事件
 	watcher
@@ -99,54 +41,14 @@ io.on('connection',function(socket){
 		})
 		.on('change', function (file) {
 			log.info("File:" + file + " has been changed");
-			if(resourceOffice[file] && resourceOffice.length!=0 && util.contains(resourceOffice[file],socket.name)){
-				socket.emit("change",{file:path.basename(file),source:file});
-			}
+			socket.emit("change",{name:path.basename(file),path:file});
 		})
 		.on('unlink', function (file) {
 			log.info("File:" + file + " has been removed");
 		});
 
-	//connect之后再客户端生成uuid然后触发register事件在服务器上注册
-	socket.on('register',function(data){
-
-		log.debug("in register!");
-
-		//socket.name就是pageId,并且将成为辨识每一个客户端的符号
-		socket.name=data.pageId;
-
-		//处理pageOffice和resourceOffice相关
-		addPage(data);
-
-		//为watcher添加相关的文件
-		for (var i = 0,len=data.fileList.length; i < len; i++) {
-			var fileItem = data.fileList[i];
-			watcher.add(fileItem);
-		}
-
-		log.debug("pageOffice"+JSON.stringify(pageOffice));
-		log.debug("resourceOffice:"+JSON.stringify(resourceOffice));
-	});
-
 	socket.on('disconnect',function(){
-
 		log.debug("disconnecting!");
-
-		//处理pageOffice和resourceOffice相关
-		removePage(socket.name);
-
-		//处理watcher,如果没有需要该文件的页面,那么就执行unwatch
-		for (var fileItem in resourceOffice) {
-			var pageList = resourceOffice[fileItem];
-			if(util.isArray(pageList)&&pageList.length===0){
-				watcher.unwatch(fileItem);
-				log.debug("unwatch file:"+fileItem);
-				delete resourceOffice[fileItem];
-			}
-		}
-
-		log.debug("pageOffice"+JSON.stringify(pageOffice));
-		log.debug("resourceOffice:"+JSON.stringify(resourceOffice));
 	});
 });
 
